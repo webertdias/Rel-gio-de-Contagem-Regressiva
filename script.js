93da9e0dd51a63e0b240c404d1c24ab4a081f50e
Webert Dias <wdwebert@gmail.com>
2023-03-05 19:24:33 -0300

    Iniciando javascript no projeto

---
+++ script.js
@@ -0,0 +1,5 @@
+const dias = document.getElementById("dias");
+const horas = document.getElementById("horas");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
+
