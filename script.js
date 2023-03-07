138ed8c766580b32142aad093c54fd2d5c84496e
Webert Dias <wdwebert@gmail.com>
2023-03-06 21:59:20 -0300

    Desenvolvendo o js no projeto

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
 
+let dataFutura = new Date("2023, 4, 16, 8, 0, 0");
+const dataAtual = new Date()
+
+function seg(){
+    for (let i = 60; i >= 0; i--) {
+        var seg = setInterval*i;
+        document.getElementById("segundos").innerText = seg;
+    }
+}
+
+    
+
+
+
+//function dia(dataAtual) {
+//    dia = (dataFutura.getDay) - (dataAtual.getDay);
+//    dia.getElementById("dias").innerHTML;
+//}
