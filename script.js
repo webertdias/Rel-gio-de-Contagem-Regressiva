b5cbcb0b8aac475be2e36f82d83a408e96f643c6
Webert Dias <wdwebert@gmail.com>
2023-03-07 10:32:24 -0300

    Cronometro funcionando

--- script.js
+++ script.js
@@ -3,15 +3,46 @@ const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
 
-let dataFutura = new Date("2023, 4, 16, 8, 0, 0");
-const dataAtual = new Date()
+const novaData = "2023, 4 ,16,";
 
-function seg(){
-    for (let i = 60; i >= 0; i--) {
-        var seg = setInterval*i;
-        document.getElementById("segundos").innerText = seg;
-    }
+function cronometro(){
+
+    const dataFutura = new Date(novaData);
+    const dataAtual = new Date();
+
+    const segundostotais = (dataFutura - dataAtual)/1000;
+
+    const dia = Math.floor(segundostotais / 3600 /24);
+    const hora = Math.floor(segundostotais/3600) % 24;
+    const minuto = Math.floor(segundostotais /60) % 60;
+    const segundo = Math.floor(segundostotais) % 60;
+
+    dias.innerHTML = dia;
+    horas.innerHTML = relogio(hora);
+    minutos.innerHTML = relogio(minuto);
+    segundos.innerHTML = relogio(segundo);
+
 }
+
+function relogio(rel){
+    return rel < 10 ? `${rel}` : rel;
+}
+
+cronometro();
+
+setInterval(cronometro, 1000);
+
+
+
+
+
+/* function seg(){
+    for (let i = 60; i >= 0; i--) {
+        document.getElementById("segundos").innerHTML += i;
+        clearInterval(i);
+
+ }
+} */
 
     
 
